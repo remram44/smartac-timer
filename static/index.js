de1f562b75fabc77b1de624245d4ad6d815cf4e4
Remi Rampin <r@remirampin.com>
2020-08-31 14:19:32 -0400

    Use reconciliation

--- static/index.js
+++ static/index.js
@@ -1,5 +1,36 @@
-function entriesMatch(a, b) {
-  return a.id == b.id;
+function sortByKey(array, key) {
+  array.sort(function(a, b) {
+    if(key(a) < key(b)) {
+      return -1;
+    } else if(key(a) > key(b)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+function objEqual(obj1, obj2) {
+  var props1 = Object.getOwnPropertyNames(obj1);
+  var props2 = Object.getOwnPropertyNames(obj2);
+
+  if(props1.length != props2.length) {
+    return false;
+  } else {
+    for(var i = 0; i < props1.length; i++) {
+      var prop = props1[i];
+
+      if(typeof(obj1[prop]) === 'object' && typeof(obj2[prop]) === 'object') {
+        if(!objEqual(obj1[prop], obj2[prop])) {
+          return false;
+        }
+      } else if(obj1[prop] !== obj2[prop]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 function reconcileList(container, currentList, newList, buildFn) {
@@ -20,7 +51,7 @@ function reconcileList(container, currentList, newList, buildFn) {
         container.insertBefore(newChild, currentElement);
         newIdx++;
       } else {
-        if(entriesMatch(currentList[currentIdx], newList[newIdx])) {
+        if(objEqual(currentList[currentIdx], newList[newIdx])) {
           // Entries match, move on
           currentIdx++;
           newIdx++;
@@ -67,55 +98,65 @@ function setDeviceTimer(mode, deviceId, minutes) {
   }));
 }
 
-var req = new XMLHttpRequest();
-req.responseType = 'json';
-req.open('GET', '/api/status', true);
-req.onload = function() {
-  var obj = req.response;
-  var devices = document.getElementById('devices');
-  devices.innerHTML = '';
-  for(var i = 0; i < obj.devices.length; ++i) {
-    var d = obj.devices[i];
-    var inv;
-    if(d.status === 'on') {
-      inv = 'off';
-    } else {
-      inv = 'on';
-    }
-    var elem = document.createElement('tr');
-    var h = (
-      '<td>' + d.name + '</td>' +
-      '<td><a href="javascript:switchDevice(\'' + inv + '\', ' + d.id + ')">' + d.status + '</a></td>'
-    );
-    if(d.temperature) {
-      h += (
-        '<td>current: ' + d.temperature.farenheit + '°F</td>'
-      );
-    } else {
-      h += '<td></td>';
-    }
-    if(d.thermostat) {
-      h += (
-        '<td>target: ' + d.thermostat.farenheit + '°F</td>'
-      );
-    } else {
-      h += '<td></td>';
-    }
-    if(d.timer) {
-      h += (
-        '<td>turn ' + d.timer.mode + ' at ' + d.timer.when + '</td>'
-      );
-    } else {
-      h += '<td></td>';
-    }
-    h += (
-      '<td>Turn ' + inv + ' in ' +
-      '<a href="javascript:setDeviceTimer(\'' + inv + '\', ' + d.id + ', ' + (30) + ')">30min</a> ' +
-      '<a href="javascript:setDeviceTimer(\'' + inv + '\', ' + d.id + ', ' + (60) + ')">1hr</a> ' +
-      '</td>'
-    );
-    elem.innerHTML = h;
-    devices.appendChild(elem);
+function renderDeviceRow(d) {
+  var inv;
+  if(d.status === 'on') {
+    inv = 'off';
+  } else {
+    inv = 'on';
   }
-};
-req.send();
+  var elem = document.createElement('tr');
+  var h = (
+    '<td>' + d.name + '</td>' +
+    '<td><a href="javascript:switchDevice(\'' + inv + '\', ' + d.id + ')">' + d.status + '</a></td>'
+  );
+  if(d.temperature) {
+    h += (
+      '<td>current: ' + d.temperature.farenheit + '°F</td>'
+    );
+  } else {
+    h += '<td></td>';
+  }
+  if(d.thermostat) {
+    h += (
+      '<td>target: ' + d.thermostat.farenheit + '°F</td>'
+    );
+  } else {
+    h += '<td></td>';
+  }
+  if(d.timer) {
+    h += (
+      '<td>turn ' + d.timer.mode + ' at ' + d.timer.when + '</td>'
+    );
+  } else {
+    h += '<td></td>';
+  }
+  h += (
+    '<td>Turn ' + inv + ' in ' +
+    '<a href="javascript:setDeviceTimer(\'' + inv + '\', ' + d.id + ', ' + (30) + ')">30min</a> ' +
+    '<a href="javascript:setDeviceTimer(\'' + inv + '\', ' + d.id + ', ' + (60) + ')">1hr</a> ' +
+    '</td>'
+  );
+  elem.innerHTML = h;
+  return elem;
+}
+
+var currentStatus = undefined;
+
+function updateStatus() {
+  var req = new XMLHttpRequest();
+  req.responseType = 'json';
+  req.open('GET', '/api/status', true);
+  req.onload = function() {
+    var newStatus = req.response;
+    sortByKey(newStatus.devices, function(d) { return d.id; });
+    var devices = document.getElementById('devices');
+    if(currentStatus === undefined) {
+      devices.innerHTML = '';
+      currentStatus = {devices: []};
+    }
+    reconcileList(devices, currentStatus.devices, newStatus.devices, renderDeviceRow);
+    currentStatus = newStatus;
+  };
+  req.send();
+}
